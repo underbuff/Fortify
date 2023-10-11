debb7783c49e68b4f93ef120bba9b073ccbe570e
Oleg Safonov <osafonov@ibagroup.eu>
2023-10-11 20:26:11 +0200

    fix: unused imports

--- services/frontend/components/footer.tsx
+++ services/frontend/components/footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
 	return (
--- services/frontend/components/navbar.tsx
+++ services/frontend/components/navbar.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 
-import Image from "next/image";
-
 import Link from "next/link";
 import { useRouter } from "next/router";
 
